492d345ea6ee459a2c718f588b16356b6c104a77
odem <odem@noobnerd.net>
2021-12-28 15:58:18 +0100

    Add default shorewall configuration

--- scripts/components/mps-comp-terminal-essentials.sh
+++ scripts/components/mps-comp-terminal-essentials.sh
@@ -14,11 +14,29 @@ PACKAGES="$PACKAGES sudo dconf-cli dconf-editor dbus-x11 psmisc cron nload"
 PACKAGES="$PACKAGES gcc build-essential gawk"
 
 # Networking
-PACKAGES="$PACKAGES nmap tcpdump dnsutils net-tools whois ssh"
+PACKAGES="$PACKAGES nmap tcpdump dnsutils net-tools whois ssh iptables shorewall"
+
+# Security
+PACKAGES="$PACKAGES iptables shorewall"
 
 $INSTALLER_CMD $PACKAGES $INSTALLER_OPT
 
+if [[ -d files/etc/shorewall ]] ; then
+    if [[ -d /etc/shorewall ]] ; then
+        sudo cp files/etc/shorewall/interfaces /etc/shorewall/
+        sudo cp files/etc/shorewall/policy /etc/shorewall/
+        sudo cp files/etc/shorewall/rules /etc/shorewall/
+        sudo cp files/etc/shorewall/zones /etc/shorewall/
+    else
+        echo "DstPath not valid! Exiting now.."
+        exit 1
+    fi
+else
+    echo "SrcPath not valid! Exiting now.."
+    exit 1
+fi
 
+sudo shorewall restart
 
 
 
